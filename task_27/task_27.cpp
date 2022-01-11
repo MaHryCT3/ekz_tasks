22a4b40b352af5031308d5f66f0ae46bad1b68cf
MaHryCT3 <mahryct123@gmail.com>
2022-01-11 15:20:13 +0300

    add 27

---
+++ task_27/task_27.cpp
@@ -0,0 +1,34 @@
+﻿#include <windows.h>
+#include <iostream>
+
+/*Задача 27
+
+Написать рекурсивную функцию (не использовать циклов) для
+многократного вывода символьной строки. Строка — параметр функции.
+
+В main ‚ используя эту функцию!10 раз вывести на экран произвольную фразу.
+
+*/
+
+using namespace std;
+
+char ShowStrings(char *phrase, int count) {
+    if (count - 1 > 0) {
+        cout << phrase << endl;
+        return ShowStrings(phrase, count - 1);
+    }
+    else {
+        cout << phrase;
+        return 0;
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    char phrase[] = "[eq pfkegf xkty";
+
+    ShowStrings(phrase, 10);
+
+}
